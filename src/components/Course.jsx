d626305085b24d0b249e90384838748fcdd17534
faizhaidarsa <faizhaidarsa@gmail.com>
2019-09-12 14:40:16 +0700

    map jakarta

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -20,7 +20,7 @@ export class Course extends Component {
             foto:'images/info-logo-circle_318-947.jpg'
           },{
               id:2,
-            nama:'Ir. Aladin Rapi Saenalm, MM',
+            nama:'Ir. Aladin Rapi Saenal, MM',
             deskripsi:'a NDT professional for aircraft who has performed extensive appplication work in the Garuda Maintenance Facility including: Boeing-747, B737 series, B737NG, A330, A-320 etc. He is certified as NDT-Level III. He has been involved in submitting ASNT conference in US and training to various industrial sectors. He has been responsible for the professional training programs in the fields of welding, casting, airframe, NDT standards etc at National Standardization Bard and Indonesian Societies of NDT. He is also Auditor ISO-17020 on behalf of National Accreditation Board. Presently, he is a NDT consultant, Trainer and Examiner',
             foto:'images/info-logo-circle_318-947.jpg'
         },{
