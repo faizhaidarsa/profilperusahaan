c2c7792184fc688e443f606b32368d959a72b61b
faizhaidarsa <faizhaidarsa@gmail.com>
2019-09-12 15:08:13 +0700

    client

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -19,7 +19,12 @@ export class Client extends Component {
             <div className='container'>
             <div style={{height:this.state.height}}></div>
             <h1 className='display-4 text-center mb-3'><strong>Our Client</strong></h1>
-                
+                <Row>
+                    <Col className='p-4 d-flex align-items-center justify-content-center' xs='12' sm='6' md='4'><img width='70%' src="images/1920px-GMF_AeroAsia_logo.svg.png" alt=""/></Col>
+                    <Col className='p-4 d-flex align-items-center justify-content-center' xs='12' sm='6' md='4'><img width='70%' src="images/1280px-LOGO_SJ_VERTIKAL.png" alt=""/></Col>
+                    <Col className='p-4 d-flex align-items-center justify-content-center' xs='12' sm='6' md='4'><img width='70%' src="images/di.png" alt=""/></Col>
+                    <Col className='p-4 d-flex align-items-center justify-content-center' xs='12' sm='6' md='4'><img width='70%' src="images/suco.png" alt=""/></Col>
+                </Row>
             </div>
         )
     }
