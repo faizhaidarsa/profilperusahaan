28f23d85644f3864bdacb2b6a6de61c07cb9f6cf
faizhaidarsa <faizhaidarsa@gmail.com>
2019-09-23 01:56:11 +0700

    fon size

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,8 +15,8 @@ export class Home extends Component {
             <div id='homenya' className='container-fluid bg-secondary d-flex align-items-center justify-content-center' style={{height:this.state.height}}>
                 
                 <div className='row'>
-                <div className='col-12'><h1 className='display-4 text-center text-white'><strong>Non-Destructive Testing (NDT)</strong></h1></div>
-                <div className='col-12'><h1 className='h-4 text-center text-white'>SERVICE AND TRAINING</h1></div>
+                <div className='col-12'><h1 className='text-center text-white'><strong>Non-Destructive Testing (NDT)</strong></h1></div>
+                <div className='col-12'><h2 className='h-4 text-center text-white'>SERVICE AND TRAINING</h2></div>
                 
                     <div className='col-6 mx-auto mt-5'>
                         <Link to='/sub' className='btn btn-success btn-block'>
