03840a2f7bbce0d63d533309cba9548a22b8df1a
faizhaidarsa <faizhaidarsa@gmail.com>
2019-09-26 15:11:32 +0700

    deploy config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, HashRouter} from 'react-router-dom'
 import Main from './Main'
 import Sub from './Sub'
 import Course from './Course/Course'
@@ -12,12 +12,12 @@ export class App extends Component {
   render() {
     return (
       <div>
-      <BrowserRouter>
+      <HashRouter>
         <Route exact path='/' component={Main}/>
         <Route path='/sub' component={Sub}/>
         <Route path='/sub/course' component={Course}/>
         <Route path='/sub/consultant' component={Consultant}/>       
-      </BrowserRouter>
+      </HashRouter>
       </div>
     )
   }
